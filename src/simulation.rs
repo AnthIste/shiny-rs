d5e4997f38ded7069fb7f0680bcbfb163a4f62e2
ruben.bakker <ruben.bakker@dariel.co.za>
2014-09-04 14:06:13 +0200

    Differentiate Particle / Particle2D traits and implement for TriangleParticle

--- src/simulation.rs
+++ src/simulation.rs
@@ -4,7 +4,19 @@ pub trait Particle {
 	fn update(&mut self, dt: f32);
 }
 
+pub trait Particle2D {
+	fn new(acc: Vec2<f32>, vel: Vec2<f32>, pos: Vec2<f32>) -> Self;
+
+	fn acc(&self) -> Vec2<f32>;
+
+	fn vel(&self) -> Vec2<f32>;
+
+	fn pos(&self) -> Vec2<f32>;
+}
+
 pub trait ParticleSystem {
+	fn spawn(&mut self);
+
 	fn update(&mut self, dt: f32);
 }
 
@@ -25,21 +37,58 @@ impl TriangleParticle {
 impl Particle for TriangleParticle {
 	fn update(&mut self, dt: f32) {
 		let acc = Vec2 { x: 0f32, y: 0f32 };
-
-		// Acceleration
 		self.vel = self.vel + acc * dt;
-
-		// Velocity
 		self.pos = self.pos + self.vel * dt;
 	}
 }
 
-pub struct ScatterSystem<T: Particle> {
+impl Particle2D for TriangleParticle {
+	fn new(acc: Vec2<f32>, vel: Vec2<f32>, pos: Vec2<f32>) -> TriangleParticle {
+		TriangleParticle {
+			pos: Vec2 { x: 0f32, y: 0f32 },
+			vel: Vec2 { x: 0f32, y: 0f32 },
+		}
+	}
+
+	fn acc(&self) -> Vec2<f32> {
+		Vec2 { x: 0f32, y: 0f32 }
+	}
+
+	fn vel(&self) -> Vec2<f32> {
+		self.vel
+	}
+
+	fn pos(&self) -> Vec2<f32> {
+		self.pos
+	}
+}
+
+pub struct ScatterSystem<T> {
 	ps: Vec<T>,
 }
 
-impl<T: Particle> ParticleSystem for ScatterSystem<T> {
+impl<T> ScatterSystem<T> {
+	pub fn new() -> ScatterSystem<T> {
+		ScatterSystem {
+			ps: Vec::new(),
+		}
+	}
+}
+
+impl<T: Particle + Particle2D> ParticleSystem for ScatterSystem<T> {
+	fn spawn(&mut self) {
+		let initial_acc = Vec2 { x: 0f32, y: 0f32 };
+		let initial_vel = Vec2 { x: 0f32, y: 0f32 };
+		let initial_pos = Vec2 { x: 0f32, y: 0f32 };
+		
+		let new_particle = Particle2D::new(initial_acc, initial_vel, initial_pos);
+
+		self.ps.push(new_particle);
+	}
+
 	fn update(&mut self, dt: f32) {
-		// NOP
+		for p in self.ps.mut_iter() {
+			p.update(dt);
+		}
 	}
 }
